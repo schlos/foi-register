2ddac7feada1ba23fbe607027c343a63717cf086
Robin Houston <robin.houston@gmail.com>
2012-05-21 14:26:16 +0100

    Error message on failed admin login
    Show an error message if the email address or password is wrong,
    on the admin login.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
       session[:staff_member_id] = staff_member.id
       redirect_to "/admin/requests", :notice => "Logged in"
     else
-      flash.now.alert = "Invalid email or password"
+      flash[:notice] = "Invalid email or password"
       render "new"
     end
   end
