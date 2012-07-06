a7fc972bbe38f662356896a15b02a6d2eef0b660
Seb Bacon <seb.bacon@gmail.com>
2012-07-06 12:45:20 +0100

    Ensure we're always using the TEST env, and small optimisation for spork preloading

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,15 +1,15 @@
-require 'rubygems'
 require 'spork'
 #uncomment the following line to use spork with the debugger
 #require 'spork/ext/ruby-debug'
+ENV["RAILS_ENV"] = "test"
 
 Spork.prefork do
   # Loading more in this block will cause your tests to run faster. However,
   # if you change any configuration or code from libraries loaded here, you'll
   # need to restart spork for it take effect.
-  ENV["RAILS_ENV"] = "test"
   require File.expand_path('../../config/environment', __FILE__)
   require 'rails/test_help'
+  require 'action_view/base'
 end
 
 Spork.each_run do
