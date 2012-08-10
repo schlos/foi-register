477eadd55c435eaf1e6cf7805b00f982d7257439
Robin Houston <robin.houston@gmail.com>
2012-08-10 19:20:12 +0100

    Restructure to reduce indentation

--- extras/alaveteli_api.rb
+++ extras/alaveteli_api.rb
@@ -9,36 +9,36 @@ class AlaveteliApi
 
     def self.send_request(request)
         api_endpoint = MySociety::Config::get("ALAVETELI_API_ENDPOINT")
-        if !api_endpoint.nil?
-            data = {:title => request.title,
-                :body => request.body,
-                :external_user_name => request.requestor_name,
-                :external_url => Rails.application.routes.url_helpers.request_url(nil, request, :host => MySociety::Config.get("DOMAIN", "localhost:3000"))
-            }.to_json
-            key = MySociety::Config::get("ALAVETELI_API_KEY")
-            url = URI.parse("#{api_endpoint}/request.json")
-            req = Net::HTTP::Post::Multipart.new(url.path,
-                                                 :k => key,
-                                                 :request_json => data)
+        return nil, nil if api_endpoint.nil?
+        
+        data = {:title => request.title,
+            :body => request.body,
+            :external_user_name => request.requestor_name,
+            :external_url => Rails.application.routes.url_helpers.request_url(nil, request, :host => MySociety::Config.get("DOMAIN", "localhost:3000"))
+        }.to_json
+        key = MySociety::Config::get("ALAVETELI_API_KEY")
+        url = URI.parse("#{api_endpoint}/request.json")
+        req = Net::HTTP::Post::Multipart.new(url.path,
+                                             :k => key,
+                                             :request_json => data)
 
-            response = Net::HTTP.start(url.host, url.port) do |http|
-                http.request(req)
-            end
-            case response
-            when Net::HTTPSuccess
-              json = ActiveSupport::JSON.decode(response.body)
-              if json['errors'].nil?
-                  Rails.logger.info("Created new request at #{json['url']}")
-                  return json['id'], json['url']
-              else
-                  raise AlaveteliApiError, json['errors']
-              end
-            when Net::HTTPUnauthorized
-              raise AlaveteliApiError, "Unauthorized: is the API key correct?"
-            else
-              Rails.logger.error("Alaveteli API error: " + response.body)
-              raise AlaveteliApiError, "Error from Alaveteli API: see log for details"
-            end
+        response = Net::HTTP.start(url.host, url.port) do |http|
+            http.request(req)
+        end
+        case response
+        when Net::HTTPSuccess
+          json = ActiveSupport::JSON.decode(response.body)
+          if json['errors'].nil?
+              Rails.logger.info("Created new request at #{json['url']}")
+              return json['id'], json['url']
+          else
+              raise AlaveteliApiError, json['errors']
+          end
+        when Net::HTTPUnauthorized
+          raise AlaveteliApiError, "Unauthorized: is the API key correct?"
+        else
+          Rails.logger.error("Alaveteli API error: " + response.body)
+          raise AlaveteliApiError, "Error from Alaveteli API: see log for details"
         end
     end
 
