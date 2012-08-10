580f25ea3ecf13b2ca5394435c43c4444a617101
Robin Houston <robin.houston@gmail.com>
2012-08-10 15:51:57 +0100

    Increase number of users per page

--- app/controllers/requestors_controller.rb
+++ app/controllers/requestors_controller.rb
@@ -4,7 +4,7 @@ class RequestorsController < ApplicationController
   # GET /requestors
   # GET /requestors.json
   def index
-    @requestors = Requestor.paginate(:page => params[:page], :per_page => 5).all
+    @requestors = Requestor.paginate(:page => params[:page], :per_page => 100).all
 
     respond_to do |format|
       format.html # index.html.erb
