32a80d5ed42a6180e1b4fa4d806afb1abf3a8907
Robin Houston <robin.houston@gmail.com>
2012-09-12 14:19:01 +0100

    Correction to acknowledgement mailer
    You can’t set instance variables that way, in Ruby.

--- app/mailers/request_mailer.rb
+++ app/mailers/request_mailer.rb
@@ -2,10 +2,9 @@ class RequestMailer < ActionMailer::Base
   default :from => MySociety::Config.get("ORG_EMAIL")
   default :subject => MySociety::Config.get("ACKNOWLEDGEMENT_SUBJECT")
   
-  @notifications_to = MySociety::Config.get("NOTIFICATIONS_TO")
-  @notification_subject = MySociety::Config.get("NOTIFICATION_SUBJECT")
-  
   def notification(request)
+    @notifications_to ||= MySociety::Config.get("NOTIFICATIONS_TO")
+    @notification_subject ||= MySociety::Config.get("NOTIFICATION_SUBJECT")
     @request = request
     mail(:to => @notifications_to, :subject => @notification_subject)
   end
